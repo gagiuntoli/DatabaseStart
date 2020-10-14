639750d964e5d7c344f52b15fba1cd73527454f9
Guido Giuntoli <gagiuntoli@gmail.com>
2020-10-14 21:14:03 +0200

    Adding the basics of the front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>CRUD APPLICATION</h1>
+class App extends Component {
 
-      <div className="form">
-        <label>Word in German</label>
-        <input type="text" name="german" />
-        <label>Word in English</label>
-        <input type="text" name="english" />
-        <button>Submit</button>
+  constructor() {
+    super();
+    this.state = {
+      german: "",
+      english: ""
+    }
+  }
+
+  handleGermanWord = (e) =>{
+    this.setState({
+      german: e.target.value
+    })
+    console.log('german:' + this.state.german)
+  }
+
+  handleEnglishWord = (e) =>{
+    this.setState({
+      english: e.target.value
+    })
+  }
+
+  submitRequest = (e) => {
+    axios.post('http://localhost:3001/api/insert',
+     {
+       german: this.state.german,
+       english: this.state.english
+     }).then(()=>{
+       alert('succesful insert')
+     })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>CRUD APPLICATION</h1>
+
+        <div className="form">
+          <label>Word in German</label>
+          <input type="text" name="german" onChange={this.handleGermanWord} value={this.state.german}/>
+          <label>Word in English</label>
+          <input type="text" name="english" onChange={this.handleEnglishWord} value={this.state.english}/>
+          <button onClick={this.submitRequest}>Submit</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
